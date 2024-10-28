7b3956428b506c1d39cb36a3fcb8f17b3d8e0eb3
Darkside <sduchd@gmail.com>
2024-10-28 13:41:07 +0800

    remove unused files

--- qr_login_python/django/exec.ps1
+++
@@ -1,2 +0,0 @@
-docker build -t p1 .
-docker run --env-file .env -it -p 3000:3000 --name container1 p1
--- qr_login_react/src/reducer/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import { qrlogin } from './qrlogin.redux';
-
-
-
-export default combineReducers({ qrlogin });
